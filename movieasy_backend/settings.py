032ec3cf729c266e4b41dce36a57d3544ab93c29
Guntak Kim <caputdraconis@kakao.com>
2022-11-22 09:13:17 +0900

    chore: move secret_key to env

--- movieasy_backend/settings.py
+++ movieasy_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lepy+67$x-9l7fq)2c7^jxy-c$(2+m4mjkycnr2l$=)wl7ja9b'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
