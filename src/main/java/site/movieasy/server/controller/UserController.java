81cdb8a243222310dce574af1cc63b4fa7ac0231
Guntak Kim <caputdraconis@kakao.com>
2022-12-03 15:30:44 +0900

    feat: add User Controller

---
+++ src/main/java/site/movieasy/server/controller/UserController.java
@@ -0,0 +1,14 @@
+package site.movieasy.server.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("api/user")
+@RestController
+public class UserController {
+    @PostMapping("/register")
+    public void register() {
+
+    }
+}
